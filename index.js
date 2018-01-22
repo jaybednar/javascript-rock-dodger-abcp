09f7db857d4b3848809a343b69b2f3625843d110
Jay Bednar <ironviralmedia@gmail.com>
2018-01-22 22:57:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -141,6 +141,10 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   if (e.which === LEFT_ARROW) {
+     e.stopPropagation();
+     e.preventDefault();
+   }
 }
 
 function moveDodgerLeft() {
